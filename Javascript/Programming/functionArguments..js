ebb63ca02ae275009fac88fba4f1c975495f9d67
Techie-Surya <surya14dash@gmail.com>
2025-09-01 00:51:54 +0530

    javascript

---
+++ Javascript/Programming/functionArguments..js
@@ -0,0 +1,6 @@
+function printInfo(name,age){
+  console.log(`${name}'s age is ${age}.`);
+}
+
+printInfo("kunal",19);
+printInfo("surya",20);
---
+++ Javascript/Programming/return.js
@@ -0,0 +1,8 @@
+function sum(a,b){
+  return a+b;
+}
+
+let s = sum(3,5);
+let t = sum(6,14);
+console.log(s);
+console.log(t);
