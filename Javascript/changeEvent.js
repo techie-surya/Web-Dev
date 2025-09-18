3c287e57d222a3eb20dab0658b2a1f3213d49660
Techie-Surya <surya14dash@gmail.com>
2025-09-18 21:31:05 +0530

    javascript

---
+++ Javascript/changeEvent.js
@@ -0,0 +1,12 @@
+let form = document.querySelector("form");
+
+form.addEventListener("submit",function(event) {
+  event.preventDefault();
+});
+
+let user =  document.querySelector("#user");
+
+user.addEventListener("change", function() {
+  console.log("input changed");
+  console.log("final value =", this.value);
+});
