51f1c343dcb0d7c23a6a40aff35100b6b6e7556f
Techie-Surya <surya14dash@gmail.com>
2025-11-02 19:13:19 +0530

    node.js

---
+++ Node.js/Export Module/export2(require).js
@@ -0,0 +1,16 @@
+//Export from Different Directory
+
+let info = require("../Fruits");
+
+console.log(info);
+
+//In TERMINAL
+
+// PS A:\WebDev-AC\Node.js> node export2.js
+
+//OUTPUT:
+// [
+//   { name: 'apple', color: 'red' },
+//   { name: 'orange', color: 'orange' },
+//   { name: 'mango', color: 'yellow' }
+// ]
---
+++ Node.js/import.js
@@ -0,0 +1,4 @@
+import { sum, PI} from "./math(import).js";
+
+console.log(sum(2, 5));
+console.log(4 * PI);
